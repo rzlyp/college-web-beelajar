83a8f4bef5a7ac9a19bfe3350310d24853d0db7a
Rizal Yogi Pratama <rizalyogip@gmail.com>
2017-07-20 05:37:00 +0700

    add api

--- app/controller/api/log.js
+++ app/controller/api/log.js
@@ -3,7 +3,7 @@ const db = require('../../../config/database');
 function LogController(){
 	this.getLogMurid = (req, res, next) => {
 		db.getConnection((err, con) => {
-			con.query("SELECT pengajar.nama_pengajar, customer.nama_customer ,jadwal_mengajar.materi_mengajar , log_mengajar.mulai_mengajar, log_mengajar.akhir_mengajar FROM jadwal_mengajar INNER JOIN pengajar ON pengajar.id_pengajar = jadwal_mengajar.id_pengajar INNER JOIN customer ON customer.id_customer = jadwal_mengajar.id_customer INNER JOIN log_mengajar ON log_mengajar.id_jadwal_mengajar = jadwal_mengajar.id_jadwal_mengajar WHERE jadwal_mengajar.id_customer = ? ", req.params.id_customer, (err,log) =>{
+			con.query("SELECT pengajar.id_pengajar,customer.id_customer ,pengajar.nama_pengajar, customer.nama_customer, pengajar.foto_pengajar ,jadwal_mengajar.materi_mengajar , log_mengajar.mulai_mengajar, log_mengajar.akhir_mengajar FROM jadwal_mengajar INNER JOIN pengajar ON pengajar.id_pengajar = jadwal_mengajar.id_pengajar INNER JOIN customer ON customer.id_customer = jadwal_mengajar.id_customer INNER JOIN log_mengajar ON log_mengajar.id_jadwal_mengajar = jadwal_mengajar.id_jadwal_mengajar WHERE jadwal_mengajar.id_customer = ? ", req.params.id_customer, (err,log) =>{
 			con.release();	
 				if(err)
 					console.log(err);
